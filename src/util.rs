ea8fd662b799e1fdd2af4efb8523c923ee2098ae
Gedalia Kott <gedkott@gmail.com>
2021-01-12 16:03:48 -0800

    remove unused function

--- src/util.rs
+++ src/util.rs
@@ -8,12 +8,6 @@ pub fn read_be_u32(input: &mut &[u8]) -> Result<u32, std::array::TryFromSliceErr
     int_bytes.try_into().map(u32::from_be_bytes)
 }
 
-pub fn read_be_i32(input: &mut &[u8]) -> Result<i32, std::array::TryFromSliceError> {
-    let (int_bytes, rest) = input.split_at(std::mem::size_of::<i32>());
-    *input = rest;
-    int_bytes.try_into().map(i32::from_be_bytes)
-}
-
 pub fn attach_bytes(bytes: &[std::slice::Iter<'_, u8>]) -> Vec<u8> {
     bytes.iter().cloned().flatten().cloned().collect()
 }
